dcb04e46983abed9d9eaf75bdf886502103dc826
Dion Henare <dionhenare@gmail.com>
2019-03-15 10:01:24 +0100

    makes long table of all data and plots ERP

--- plotERPs.R
+++ plotERPs.R
@@ -8,7 +8,8 @@ conditions = c("epochInfo$Event==\"Search\" & epochInfo$LatStim==\"Target\" ",
 rightHemCond = '& epochInfo$Hemifield==\"Right\" '
 leftHemCond = '& epochInfo$Hemifield==\"Left\" '
 
-
+#####
+#Creates aggregate of all participant data (needs dPath and fPrefix)
 eFilePattern = paste(fPrefix,"*_epochs.csv", sep="")
 lFilePattern = paste(fPrefix,"*_LH.csv", sep="")
 rFilePattern = paste(fPrefix,"*_RH.csv", sep="")
@@ -16,18 +17,46 @@ eFileList = list.files(dPath, pattern=glob2rx(eFilePattern))
 lFileList = list.files(dPath, pattern=glob2rx(lFilePattern))
 rFileList = list.files(dPath, pattern=glob2rx(rFilePattern))
 
-for (subj in 1:length(eFileList)) {
-  epochInfo = read.csv(file = paste(dPath,eFileList[subj], sep=""))
-  lHemData = read.csv(file = paste(dPath,lFileList[subj], sep=""), header = FALSE)
-  rHemData = read.csv(file = paste(dPath,rFileList[subj], sep=""), header = FALSE)
+#create variables using first dataset
+epochInfo = read.csv(file = paste(dPath,eFileList[1], sep=""))
+epochInfo$Subject = 1
+lHemData = read.csv(file = paste(dPath,lFileList[1], sep=""), header = FALSE)
+rHemData = read.csv(file = paste(dPath,rFileList[1], sep=""), header = FALSE)
+#append the other datasets to the above variables
+for (subj in 2:length(eFileList)) {
+  curEpochInfo = read.csv(file = paste(dPath,eFileList[subj], sep=""))
+  curEpochInfo$Subject = subj
+  curLHemData = read.csv(file = paste(dPath,lFileList[subj], sep=""), header = FALSE)
+  curRHemData = read.csv(file = paste(dPath,rFileList[subj], sep=""), header = FALSE)
   
-  for (cond in 1:length(conditions)) {
-  #Extract epochs matching condition
-  contralateral = cbind(lHemData[eval(parse(text=paste(conditions[cond], rightHemCond ,sep="")))],
-                    rHemData[eval(parse(text=paste(conditions[cond], leftHemCond ,sep="")))]
-                    )
-  ipsilateral= cbind(lHemData[eval(parse(text=paste(conditions[cond], leftHemCond ,sep="")))],
-                      rHemData[eval(parse(text=paste(conditions[cond], rightHemCond ,sep="")))]
-                      )
-  }
+  epochInfo = rbind(epochInfo, curEpochInfo)
+  lHemData = rbind(lHemData, curLHemData)
+  rHemData = rbind(rHemData, curRHemData)
 }
+epochInfo$Subject = as.factor(epochInfo$Subject)
+
+#clear stuff that I don't need
+rm(curEpochInfo,curLHemData,curRHemData, fPrefix, eFileList, eFilePattern, lFileList, lFilePattern, rFileList, rFilePattern, subj)
+#####
+#combine all the data together into one long table
+library(tidyr)
+library(dplyr)
+gathercols = colnames(lHemData)
+lHemData$Hem = "Left"
+rHemData$Hem = "Right"
+scalpData = rbind(lHemData,rHemData)
+epochInfo = rbind(epochInfo,epochInfo)
+
+allData <- cbind(epochInfo, scalpData)
+allData <- gather(allData, "sample", "voltage", gathercols, factor_key = TRUE)
+
+allData$sample <- as.integer(substring(allData$sample,2))
+
+#clear stuff that I don't need
+rm(epochInfo,lHemData,rHemData,scalpData, gathercols)
+#####
+library(ggplot2)
+plot <- allData %>%
+  group_by(LatStim,sample) %>%
+  summarise(mean = mean(voltage)) %>%
+  ggplot(., aes(sample, mean)) + geom_line(aes(linetype = LatStim))

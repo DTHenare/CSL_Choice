90f9025a516a6293984e873a4b08a3eacb33d9ec
Dion Henare <dionhenare@gmail.com>
2019-03-15 11:30:46 +0100

    added group information

--- plotERPs.R
+++ plotERPs.R
@@ -1,12 +1,10 @@
 dPath = 'F:/CSL Choice/Data/EEG (raw data)/Visuals/'
 fPrefix = 'N2pc'
 
-conditions = c("epochInfo$Event==\"Search\" & epochInfo$LatStim==\"Target\" ",
-               "epochInfo$Event==\"Search\" & epochInfo$LatStim==\"Distractor\" "
-               )
-
-rightHemCond = '& epochInfo$Hemifield==\"Right\" '
-leftHemCond = '& epochInfo$Hemifield==\"Left\" '
+#Load group data
+groupdata = read.csv(file="F:/CSL Choice/Scripts/SophiaGroupAssignment_provisional.csv")
+groupdata = groupdata %>% mutate(Group = as.factor(ifelse(Group == 1, "Colour", "Shape")))
+groupdata$Reject = as.factor(groupdata$Reject)
 
 #####
 #Creates aggregate of all participant data (needs dPath and fPrefix)
@@ -20,12 +18,16 @@ rFileList = list.files(dPath, pattern=glob2rx(rFilePattern))
 #create variables using first dataset
 epochInfo = read.csv(file = paste(dPath,eFileList[1], sep=""))
 epochInfo$Subject = 1
+epochInfo$Group = groupdata$Group[1]
+epochInfo$Reject = groupdata$Reject[1]
 lHemData = read.csv(file = paste(dPath,lFileList[1], sep=""), header = FALSE)
 rHemData = read.csv(file = paste(dPath,rFileList[1], sep=""), header = FALSE)
 #append the other datasets to the above variables
 for (subj in 2:length(eFileList)) {
   curEpochInfo = read.csv(file = paste(dPath,eFileList[subj], sep=""))
   curEpochInfo$Subject = subj
+  curEpochInfo$Group = groupdata$Group[subj]
+  curEpochInfo$Reject = groupdata$Reject[subj]
   curLHemData = read.csv(file = paste(dPath,lFileList[subj], sep=""), header = FALSE)
   curRHemData = read.csv(file = paste(dPath,rFileList[subj], sep=""), header = FALSE)
   
@@ -36,7 +38,7 @@ for (subj in 2:length(eFileList)) {
 epochInfo$Subject = as.factor(epochInfo$Subject)
 
 #clear stuff that I don't need
-rm(curEpochInfo,curLHemData,curRHemData, fPrefix, eFileList, eFilePattern, lFileList, lFilePattern, rFileList, rFilePattern, subj)
+rm(curEpochInfo,curLHemData,curRHemData, fPrefix, eFileList, eFilePattern, lFileList, lFilePattern, rFileList, rFilePattern, subj, groupdata)
 #####
 #combine all the data together into one long table
 library(tidyr)
@@ -52,11 +54,18 @@ allData <- gather(allData, "sample", "voltage", gathercols, factor_key = TRUE)
 
 allData$sample <- as.integer(substring(allData$sample,2))
 
+allData <- allData %>% mutate(Contra = as.factor(ifelse(Hemifield==Hem, "Ipsilateral", "Contralateral")))
+allData <- recode(allData, )
 #clear stuff that I don't need
 rm(epochInfo,lHemData,rHemData,scalpData, gathercols)
 #####
 library(ggplot2)
-plot <- allData %>%
-  group_by(LatStim,sample) %>%
+allData %>%
+  filter(Event == "Search" & LatStim != "None" & Reject == 0) %>%
+  group_by(LatStim,sample,Contra,Group) %>%
   summarise(mean = mean(voltage)) %>%
-  ggplot(., aes(sample, mean)) + geom_line(aes(linetype = LatStim))
+  ggplot(., aes(sample, mean)) +
+    geom_line(aes(linetype = Contra)) +
+    facet_grid(Group~LatStim) +
+    scale_y_reverse()
+  

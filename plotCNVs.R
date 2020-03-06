1e21b5f471d1d538ad4fd1b3758ece0fd69ee585
Dion Henare <dionhenare@gmail.com>
2020-03-06 13:03:31 +0100

    added loop to run both experiments together

--- plotCNVs.R
+++ plotCNVs.R
@@ -2,9 +2,13 @@ library(tidyr)
 library(dplyr)
 library(ggplot2)
 
-dPath = 'CNV_StimOnsetWide/'
+paths = c('CNV_StimOnsetWide/','CNV_HannaStimOnsetWide/')
+groupInfos = c('SophiaGroupAssignment_provisional.csv','HannaGroupAssignment_provisional.csv')
+
+for (ind in 1:2) {
+dPath = paths[ind]
 baseline = 800
-groupdata = read.csv(file="SophiaGroupAssignment_provisional.csv")
+groupdata = read.csv(file=groupInfos[ind])
 rejCrit = 700
 
 fPrefix = 'CNV'
@@ -97,7 +101,22 @@ plotData <- allData %>%
   mutate(sample = sample-baseline) %>%
   group_by(TaskChoice, sample, Chan) %>%
   summarise(mean = mean(voltage))
-save(plotData, file = paste(dPath,"plotData.RData",sep=""))
+save(plotData, file = paste(dPath,"plotData_TaskChoice.RData",sep=""))
+
+plotData <- allData %>%
+  filter(Subject %in% Keepers) %>%
+  mutate(sample = sample-baseline) %>%
+  group_by(RepSwitch, sample, Chan) %>%
+  summarise(mean = mean(voltage))
+save(plotData, file = paste(dPath,"plotData_RepSwitch.RData",sep=""))
+
+plotData <- allData %>%
+  filter(Subject %in% Keepers) %>%
+  mutate(sample = sample-baseline) %>%
+  group_by(TaskChoice,RepSwitch, sample, Chan) %>%
+  summarise(mean = mean(voltage))
+save(plotData, file = paste(dPath,"plotData_TaskChoiceXrepswitch.RData",sep=""))
+}
 
 plotData %>%
   ggplot(., aes(sample, mean)) +
@@ -112,12 +131,28 @@ plotData %>%
   theme(panel.spacing.y = unit(2, "lines"),text= element_text(size=60))
 ggsave(paste(dPath,"CNVERPs_byGroup.pdf",sep=""),width = plotWidth, height = plotHeight*2)
 
-load('CNV_StimOnsetWide/plotData.RData')
+load('CNV_StimOnsetWide/plotData_TaskChoice.RData')
 choicePlot <- plotData
 choicePlot$Exp = "Voluntary"
-load('CNV_HannaStimOnsetWide/plotData.RData')
+load('CNV_HannaStimOnsetWide/plotData_TaskChoice.RData')
 plotData$Exp = "Cued"
 plotData <- rbind(choicePlot,plotData)
+
+plotData %>%
+  group_by(colnames(select(plotData, -Task))) %>%
+  ggplot(., aes(sample, mean)) +
+  geom_rect(xmin = -700, xmax=-500, ymin = -2, ymax = 2, size = 0, fill = "gray70", alpha = 0.05) +
+  geom_line(aes( colour = Exp, linetype = RepSwitch), size = 1.5) +
+  #scale_color_manual(values=c("#000000", "#CC0000")) +
+  scale_x_continuous(name ="Latency (ms)", expand = c(0, 0)) +
+  scale_y_reverse(name =expression(paste("Amplitude (",mu,"v)")), expand = c(0, 0)) +
+  facet_grid(Chan~.) +
+  #geom_rect(xmin = -700, xmax=-500, ymin = -Inf, ymax = Inf, size = 0, fill = "gray40", alpha = 0.1) +
+  geom_vline(xintercept = 0,linetype = "dashed" )+
+  geom_vline(xintercept = -500,linetype = "dashed" )+
+  geom_hline(yintercept = 0,linetype = "dashed") +
+  theme_minimal() 
+theme(panel.spacing.y = unit(2, "lines"),text= element_text(size=60))
 
 plotData %>%
   ggplot(., aes(sample, mean)) +

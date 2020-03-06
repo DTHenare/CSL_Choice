37ecb1ab2cfb9df7b0c390c4ccf975761fcf8d69
Dion Henare <dionhenare@gmail.com>
2020-03-06 12:29:29 +0100

    Updated a lot for revision

--- plotCNVs.R
+++ plotCNVs.R
@@ -2,11 +2,14 @@ library(tidyr)
 library(dplyr)
 library(ggplot2)
 
-dPath = 'CNV/'
+dPath = 'CNV_StimOnsetWide/'
+baseline = 800
+groupdata = read.csv(file="SophiaGroupAssignment_provisional.csv")
+rejCrit = 700
+
 fPrefix = 'CNV'
 
-#Load group data
-groupdata = read.csv(file="CNV/SophiaGroupAssignment_provisional.csv")
+#Organisevgroup data
 groupdata = groupdata %>% mutate(Group = ifelse(Group == 1, "Colour", "Shape"))
 groupdata$Reject = as.factor(groupdata$Reject)
 
@@ -59,6 +62,7 @@ epochInfo$Hemifield=NULL
 
 #clear stuff that I don't need
 rm(curEpochInfo,curFzData,curCzData,curPzData, fPrefix, eFileList, eFilePattern, fFileList, fFilePattern, cFileList, cFilePattern, pFileList, pFilePattern, subj, groupdata)
+gc()
 #####
 #combine all the data together into one long table
 gathercols = colnames(FzData)
@@ -78,6 +82,7 @@ allData$sample <- as.integer(substring(allData$sample,2))
 allData <- allData %>% mutate(RepSwitch = substring(allData$Event,15))
 allData <- allData %>% mutate(RepSwitch = ifelse((RepSwitch == "p" | RepSwitch == "ep"),"Repetition","Switch"))
 allData <- allData %>% mutate(TaskChoice = substring(Event,1,5)) %>% mutate(TaskChoice = ifelse(TaskChoice=="Searc","Search","Learn"))
+allData$Chan <- factor(allData$Chan, levels=c('Fz','Cz','Pz'))
 
 #clear stuff that I don't need
 rm(gathercols)
@@ -85,22 +90,53 @@ allData$StimTrig = NULL
 allData$Event = NULL
 gc()
 #####
-baseline = 1000
+
+#Remove bad participants
+Keepers <- allData %>% filter(Chan =="Fz", sample == 1) %>% group_by(Subject) %>% summarise(n = n()) %>% filter(n>rejCrit) %>% select("Subject")
+Keepers <- as.character(Keepers$Subject)
+
 plotWidth = 24
 plotHeight = 9
 
-allData %>%
+plotData <- allData %>%
+  filter(Subject %in% Keepers) %>%
   mutate(sample = sample-baseline) %>%
-  group_by(RepSwitch, sample, Chan, Group) %>%
-  summarise(mean = mean(voltage)) %>%
+  group_by(TaskChoice, sample, Chan) %>%
+  summarise(mean = mean(voltage))
+save(plotData, file = paste(dPath,"plotData.RData",sep=""))
+
+plotData %>%
   ggplot(., aes(sample, mean)) +
-  geom_line(aes(colour = RepSwitch),size=3) +
+  geom_line(aes(colour = TaskChoice),size=3) +
   scale_color_manual(values=c("#000000", "#CC0000")) +
   scale_x_continuous(name ="Latency (ms)", expand = c(0, 0)) +
   scale_y_reverse(name =expression(paste("Amplitude (",mu,"v)")), expand = c(0, 0)) +
-  facet_grid(Chan~Group,scales = "free_y") +
-  geom_vline(xintercept = -800,linetype = "dashed" )+
+  facet_grid(Chan~.,scales = "free_y") +
+  geom_vline(xintercept = 0,linetype = "dashed" )+
   geom_hline(yintercept = 0,linetype = "dashed") +
   theme_minimal() +
   theme(panel.spacing.y = unit(2, "lines"),text= element_text(size=60))
-ggsave("CNVERPs.pdf",width = plotWidth, height = plotHeight*2)
+ggsave(paste(dPath,"CNVERPs_byGroup.pdf",sep=""),width = plotWidth, height = plotHeight*2)
+
+load('CNV_StimOnsetWide/plotData.RData')
+choicePlot <- plotData
+choicePlot$Exp = "Voluntary"
+load('CNV_HannaStimOnsetWide/plotData.RData')
+plotData$Exp = "Cued"
+plotData <- rbind(choicePlot,plotData)
+
+plotData %>%
+  ggplot(., aes(sample, mean)) +
+  geom_rect(xmin = -700, xmax=-500, ymin = -2, ymax = 2, size = 0, fill = "gray70", alpha = 0.05) +
+  geom_line(aes( colour = Exp, linetype = RepSwitch), size = 1.5) +
+  #scale_color_manual(values=c("#000000", "#CC0000")) +
+  scale_x_continuous(name ="Latency (ms)", expand = c(0, 0)) +
+  scale_y_reverse(name =expression(paste("Amplitude (",mu,"v)")), expand = c(0, 0)) +
+  facet_grid(Chan~.) +
+  #geom_rect(xmin = -700, xmax=-500, ymin = -Inf, ymax = Inf, size = 0, fill = "gray40", alpha = 0.1) +
+  geom_vline(xintercept = 0,linetype = "dashed" )+
+  geom_vline(xintercept = -500,linetype = "dashed" )+
+  geom_hline(yintercept = 0,linetype = "dashed") +
+  theme_minimal() 
+  theme(panel.spacing.y = unit(2, "lines"),text= element_text(size=60))
+ggsave(paste(dPath,"CNVERPs.pdf",sep=""),width = plotWidth, height = plotHeight*2)

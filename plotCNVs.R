3fcf309f702be2b1f315474c84fbd446998d37e5
Dion Henare <dionhenare@gmail.com>
2020-03-10 12:04:49 +0100

    updated visuals and added stats

--- plotCNVs.R
+++ plotCNVs.R
@@ -3,7 +3,7 @@ library(dplyr)
 library(ggplot2)
 
 paths = c('CNV_StimOnsetWide/','CNV_HannaStimOnsetWide/')
-groupInfos = c('SophiaGroupAssignment_provisional.csv','HannaaGroupAssignment_provisional.csv')
+groupInfos = c('SophiaGroupAssignment_provisional.csv','HannaGroupAssignment_provisional.csv')
 
 for (ind in 1:length(paths)) {
 dPath = paths[ind]
@@ -102,82 +102,118 @@ plotHeight = 9
 plotData <- allData %>%
   filter(Subject %in% Keepers) %>%
   mutate(sample = sample-baseline) %>%
-  group_by(TaskChoice,RepSwitch, sample, Chan) %>%
+  group_by(TaskChoice,RepSwitch, sample, Chan, Subject) %>%
   summarise(mean = mean(voltage))
 save(plotData, file = paste(dPath,"plotData_TaskChoiceXrepswitch.RData",sep=""))
 
 }
 
+plotWidth = 24
+plotHeight = 9
+dPath = 'F:/CSL_Choice/CNV_StimOnsetWide/'
 load('CNV_StimOnsetWide/plotData_TaskChoiceXrepswitch.RData')
 choicePlot <- plotData
 choicePlot$Exp = "Voluntary"
+choicePlot= choicePlot %>% mutate(Subject = paste("vol",as.character(Subject),sep =""))
 load('CNV_HannaStimOnsetWide/plotData_TaskChoiceXrepswitch.RData')
 plotData$Exp = "Cued"
+plotData= plotData %>% mutate(Subject = paste("cue",as.character(Subject),sep =""))
 plotData <- rbind(choicePlot,plotData)
+
+x = c(-800,-500)
+y=c(0,0)
+lineData = data.frame(x=x,y=y)
 
 plotData %>%
   group_by(sample, Chan, Exp) %>%
   summarise(mean = mean(mean)) %>%
-  ggplot(., aes(sample, mean, colour = Exp)) +
-  geom_line(size=3) +
-  #scale_color_manual(values=c("#000000", "#CC0000")) +
+  ggplot() +
+  geom_rect(xmin = -300, xmax=-0, ymin = -Inf, ymax = Inf, size = 0, fill = "lemonchiffon") +
+  #geom_rect(xmin = -700, xmax=-500, ymin = -Inf, ymax = Inf, size = 0, fill = "gray85") +
+  geom_line(aes(sample, mean, colour = Exp),size=1) +
   scale_x_continuous(name ="Latency (ms)", expand = c(0, 0)) +
   scale_y_reverse(name =expression(paste("Amplitude (",mu,"v)")), expand = c(0, 0)) +
   facet_grid(Chan~.,scales = "free_y") +
   geom_vline(xintercept = 0,linetype = "dashed" )+
+  geom_vline(xintercept = -500)+
   geom_hline(yintercept = 0,linetype = "dashed") +
-  theme_minimal() +
+  geom_line(data = lineData,aes(x=x,y=y)) +
+  theme_minimal()
   theme(panel.spacing.y = unit(2, "lines"),text= element_text(size=60))
-ggsave(paste(dPath,"CNVERPs_Average.pdf",sep=""),width = plotWidth, height = plotHeight*2)
+ggsave(paste(dPath,"CNVERPs_Average.png",sep=""),width = 10, height = 6.49)
 
 plotData %>%
   group_by(sample, Chan, Exp, TaskChoice) %>%
   summarise(mean = mean(mean)) %>%
-  ggplot(., aes(sample, mean)) +
-  geom_rect(xmin = -700, xmax=-500, ymin = -2, ymax = 2, size = 0, fill = "gray70", alpha = 0.05) +
-  geom_line(aes( colour = Exp, linetype = TaskChoice), size = 1.5) +
-  #scale_color_manual(values=c("#000000", "#CC0000")) +
+  ggplot() +
+  geom_rect(xmin = -300, xmax=-0, ymin = -Inf, ymax = Inf, size = 0, fill = "lemonchiffon") +
+  #geom_rect(xmin = -700, xmax=-500, ymin = -Inf, ymax = Inf, size = 0, fill = "gray85") +
+  geom_line(aes(x = sample,y=mean, colour = Exp, linetype = TaskChoice), size = 1.5) +
   scale_x_continuous(name ="Latency (ms)", expand = c(0, 0)) +
   scale_y_reverse(name =expression(paste("Amplitude (",mu,"v)")), expand = c(0, 0)) +
   facet_grid(Chan~.) +
-  #geom_rect(xmin = -700, xmax=-500, ymin = -Inf, ymax = Inf, size = 0, fill = "gray40", alpha = 0.1) +
   geom_vline(xintercept = 0,linetype = "dashed" )+
-  geom_vline(xintercept = -500,linetype = "dashed" )+
+  geom_vline(xintercept = -500)+
   geom_hline(yintercept = 0,linetype = "dashed") +
+  geom_line(data = lineData,aes(x=x,y=y)) +
   theme_minimal() +
 theme(panel.spacing.y = unit(2, "lines"),text= element_text(size=60))
-ggsave(paste(dPath,"CNVERPs_TaskChoice.pdf",sep=""),width = plotWidth, height = plotHeight*2)
+ggsave(paste(dPath,"CNVERPs_TaskChoice.png",sep=""),width = plotWidth, height = plotHeight*2)
 
 plotData %>%
   group_by(sample, Chan, Exp, RepSwitch) %>%
   summarise(mean = mean(mean)) %>%
-  ggplot(., aes(sample, mean)) +
-  geom_rect(xmin = -700, xmax=-500, ymin = -2, ymax = 2, size = 0, fill = "gray70", alpha = 0.05) +
-  geom_line(aes( colour = Exp, linetype = RepSwitch), size = 1.5) +
-  #scale_color_manual(values=c("#000000", "#CC0000")) +
+  ggplot() +
+  geom_rect(xmin = -300, xmax=-0, ymin = -Inf, ymax = Inf, size = 0, fill = "lemonchiffon") +
+  #geom_rect(xmin = -700, xmax=-500, ymin = -Inf, ymax = Inf, size = 0, fill = "gray85") +
+  geom_line(aes(x = sample,y=mean, colour = Exp, linetype = RepSwitch), size = 1.5) +
   scale_x_continuous(name ="Latency (ms)", expand = c(0, 0)) +
   scale_y_reverse(name =expression(paste("Amplitude (",mu,"v)")), expand = c(0, 0)) +
   facet_grid(Chan~.) +
-  #geom_rect(xmin = -700, xmax=-500, ymin = -Inf, ymax = Inf, size = 0, fill = "gray40", alpha = 0.1) +
   geom_vline(xintercept = 0,linetype = "dashed" )+
-  geom_vline(xintercept = -500,linetype = "dashed" )+
+  geom_vline(xintercept = -500)+
   geom_hline(yintercept = 0,linetype = "dashed") +
+  geom_line(data = lineData,aes(x=x,y=y)) +
   theme_minimal() +
   theme(panel.spacing.y = unit(2, "lines"),text= element_text(size=60))
-ggsave(paste(dPath,"CNVERPs_RepSwitch.pdf",sep=""),width = plotWidth, height = plotHeight*2)
+ggsave(paste(dPath,"CNVERPs_RepSwitch.png",sep=""),width = plotWidth, height = plotHeight*2)
 
 plotData %>%
-  ggplot(., aes(sample, mean)) +
-  geom_rect(xmin = -700, xmax=-500, ymin = -2, ymax = 2, size = 0, fill = "gray70", alpha = 0.05) +
-  geom_line(aes( colour = TaskChoice, linetype = RepSwitch), size = 1.5) +
-  #scale_color_manual(values=c("#000000", "#CC0000")) +
+  group_by(sample, Chan, Exp, RepSwitch, TaskChoice) %>%
+  summarise(mean = mean(mean)) %>%
+  ggplot() +
+  geom_rect(xmin = -300, xmax=-0, ymin = -Inf, ymax = Inf, size = 0, fill = "lemonchiffon") +
+  #geom_rect(xmin = -700, xmax=-500, ymin = -Inf, ymax = Inf, size = 0, fill = "gray85") +
+  geom_line(aes( x = sample,y=mean,colour = TaskChoice, linetype = RepSwitch), size = 1) +
   scale_x_continuous(name ="Latency (ms)", expand = c(0, 0)) +
   scale_y_reverse(name =expression(paste("Amplitude (",mu,"v)")), expand = c(0, 0)) +
   facet_grid(Chan~Exp) +
-  #geom_rect(xmin = -700, xmax=-500, ymin = -Inf, ymax = Inf, size = 0, fill = "gray40", alpha = 0.1) +
   geom_vline(xintercept = 0,linetype = "dashed" )+
-  geom_vline(xintercept = -500,linetype = "dashed" )+
+  geom_vline(xintercept = -500)+
   geom_hline(yintercept = 0,linetype = "dashed") +
-  theme_minimal() +
+  geom_line(data = lineData,aes(x=x,y=y)) +
+  theme_minimal() 
   theme(panel.spacing.y = unit(2, "lines"),text= element_text(size=60))
-ggsave(paste(dPath,"CNVERPs_TaskChoiceXrepswitch.pdf",sep=""),width = plotWidth, height = plotHeight*2)
+ggsave(paste(dPath,"CNVERPs_TaskChoiceXrepswitch.png",sep=""),width = 10, height = 6.49)
+
+frontalData <- plotData %>% filter(Chan == "Fz", sample > -300, sample<0)
+posteriorData <- plotData %>% filter(Chan == "Pz", sample > -300, sample<0)
+afex::aov_ez(
+  data = frontalData,
+  dv = "mean",
+  id = "Subject", 
+  within = c("RepSwitch", "TaskChoice"),
+  between = "Exp"
+)
+afex::aov_ez(
+  data = posteriorData,
+  dv = "mean",
+  id = "Subject",
+  within = c("RepSwitch", "TaskChoice"),
+  between = "Exp"
+)
+
+frontalData <- frontalData %>% group_by(sample, Exp) %>% summarise(mean = mean(mean))
+t.test(frontalData$mean[frontalData$Exp == "Voluntary"],frontalData$mean[frontalData$Exp == "Cued"],paired = 0)
+posteriorData <- posteriorData %>% group_by(sample, Exp) %>% summarise(mean = mean(mean))
+t.test(posteriorData$mean[posteriorData$Exp == "Voluntary"],posteriorData$mean[posteriorData$Exp == "Cued"],paired = 0)
